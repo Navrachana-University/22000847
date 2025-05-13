a98034bea6833172efcf4b2038715070ddb5bd44
Dhruvv0001 <145855230+Dhruvv0001@users.noreply.github.com>
2025-05-13 15:56:57 +0530

    Add files via upload

---
+++ test_output.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+  double var_yog;
+  double var_snkh_ya1;
+  double var_snkh_ya2;
+  double t0;
+  var_snkh_ya1 = 10.0;
+  var_snkh_ya2 = 20.0;
+  t0 = var_snkh_ya1 + var_snkh_ya2;
+  var_yog = t0;
+  printf("%f\n", var_yog);
+  return 0;
+}
